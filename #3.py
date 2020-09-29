64484fe89d49cd2cfd79bb30d07be44418709816
Kritika <52737818+kritikathakkar2701@users.noreply.github.com>
2020-09-29 12:19:50 +0530

    Domino's
    You are given integers n and m representing a board of size n by m. You also have an unlimited number of 1 by 2 dominos. Return the maximum number of dominos that can be placed on the board such that they don't overlap and every domino lies entirely within the board.

---
+++ #3.py
@@ -0,0 +1,7 @@
+#You are given integers n and m representing a board of size n by m. You also have an unlimited number of 1 by 2 dominos. Return the maximum number of dominos that can be placed on the board such that they don't overlap and every domino lies entirely within the board.
+
+class Solution:
+    def solve(self, n, m):
+        size=m*n
+        num=int(0.5*size)
+        return num
