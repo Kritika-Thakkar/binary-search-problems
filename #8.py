90a29db1fed489b4bea4d45aa0cf1818551b0acf
Kritika <52737818+Kritika-Thakkar@users.noreply.github.com>
2020-10-09 21:01:25 +0530

    Sorted vowels and consonants

---
+++ #8.py
@@ -0,0 +1,16 @@
+#Given a lowercase alphabet string s, return a string with all the vowels of s in sorted order followed by all the consonants of s in sorted order.
+#Note: vowels are ["a", "e", "i", "o", "u"] and consonants are all other characters.
+
+class Solution:
+    def solve(self, s):
+        vowels = ['a','e','i','o','u'] 
+        k = ''
+        t = ''
+        for c in s:
+            if c in vowels:
+                k = k + c
+            else :
+                t = t + c
+        k = ''.join(sorted(k))
+        t = ''.join(sorted(t))
+        return k + t
