bd7e6dc57ea918afc35df5306c23c5b97c0267af
Kritika <52737818+Kritika-Thakkar@users.noreply.github.com>
2020-10-21 20:24:33 +0530

    Update #8.py

--- #8.py
+++ #8.py
@@ -1,5 +1,7 @@
 #Given a lowercase alphabet string s, return a string with all the vowels of s in sorted order followed by all the consonants of s in sorted order.
 #Note: vowels are ["a", "e", "i", "o", "u"] and consonants are all other characters.
+#Example: extraordinary
+#output: aaeiodnrrtxy
 
 class Solution:
     def solve(self, s):
