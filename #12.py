77828f29b8fbdece428bb4acd641c54bbd7001a2
Kritika <52737818+Kritika-Thakkar@users.noreply.github.com>
2020-11-09 10:57:49 +0530

    Lonely product

---
+++ #12.py
@@ -0,0 +1,17 @@
+#Given three numbers, a, b, and c, return their product, but if any two numbers are the same, they do not count
+
+class Solution:
+    def solve(self, a, b, c):
+        product = 0
+
+        if a == b == c:
+            product = 1
+        elif a == b:
+            product = c
+        elif a == c:
+            product = b
+        elif b == c:
+            product = a
+        else:
+            product = a * b * c
+        return product
