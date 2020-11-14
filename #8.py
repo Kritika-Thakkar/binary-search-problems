f0a5d4cb84645af865b5e202aa0b236361f4b71b
Kritika <52737818+Kritika-Thakkar@users.noreply.github.com>
2020-11-15 01:42:07 +0530

    Sort and Merge (strings)

--- #8.py
+++ #8.py
@@ -1,5 +1,5 @@
 #Given a lowercase alphabet string s, return a string with all the vowels of s in sorted order followed by all the consonants of s in sorted order
-#Note: vowels are ["a", "e", "i", "o", "u"] and consonants are all other characters.
+#Note: vowels are ["a", "e", "i", "o", "u"] and consonants are all other characters
 #Example: extraordinary
 #Output: aaeiodnrrtxy
 
